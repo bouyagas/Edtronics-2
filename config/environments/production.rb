7bdda316147a247b4fef1a0c0bad8881b91a6e4f
Mohamed Gassama <bouyagassama@gmail.com>
2016-09-07 02:16:32 -0400

    adding assert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,13 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  
+   config.assets.digest = true  #To access assets which are precompiled and in their names have appendend digets by rails
+   config.cache_classes = true #allowing caching assets
+ 
+config.serve_static_assets = true
+
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
